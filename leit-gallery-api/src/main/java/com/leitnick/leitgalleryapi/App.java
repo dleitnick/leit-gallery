89109820d999fedb8b7dcbc74fa3c76fe7d54bba
dleitnick <dleitnick32@gmail.com>
2023-09-02 03:27:15 -0400

    Initialized backend/frontend.

---
+++ leit-gallery-api/src/main/java/com/leitnick/leitgalleryapi/App.java
@@ -0,0 +1,13 @@
+package com.leitnick.leitgalleryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+}
